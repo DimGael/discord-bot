0a5dda6678ece9c8ee2ceac0b58f2af21ad889f2
DimGael <dimgael98@gmail.com>
2021-03-21 21:01:40 +0100

    Added help message

--- controllers/mainController.js
+++ controllers/mainController.js
@@ -1,9 +1,23 @@
+const Discord = require("discord.js");
+const config = require("../config.json")
+
+const helpMessageEmbed = new Discord.MessageEmbed()
+    .setColor('#0099ff')
+    .setTitle("Potclean Help")
+    .setURL("https://github.com/DimGael/discord-bot/")
+    .setDescription("Salut c'est moi, Potclean !\nJe ne suis pas que le nen de Knuckle, je suis aussi un bot Discord !\n"+
+        "Voici ce que je sais faire :")
+    .setThumbnail('https://www.nautiljon.com/images/perso/00/24/potclean_10642.jpg?0')
+	.addFields(
+		{ name: config.prefix+'play', value: 'je choisis à quoi vous allez jouer aujourd\'hui' },
+		{ name: config.prefix+'watch', value: 'je vous met dans une salle à l\'aise pour regarder une vidéo youtube ensemble !' },
+		{ name: config.prefix+'help', value: 'j\'affiche ce petit message tout pipou' },
+	)
+    .setFooter('Auteur du bot : Sakamoto du channel Hunters mon pote rien que ça')
+;
+
 module.exports = {
     help: function(command, args, message){
-        
-        let helpMessage = "Hello ! Je suis un bot tout nouveau ! Pour l'instant je peux juste vous aider à trouver un jeu auquel jouer si vous n'avez pas d'idée :)";
-        helpMessage += "\nFaites **g play** pour que je choisisse un jeu pour vous !";
-        message.channel.send(helpMessage);
-        
+        message.channel.send(helpMessageEmbed);
     }
 }
